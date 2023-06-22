93bcfb30d5c9f7ef3c448362e97c55d569a9e357
keni <kenikakadiya@gmail.com>
2023-06-21 21:10:52 -0700

    jay

--- foor loop pattern/p18.c
+++ foor loop pattern/p18.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int main()
 {
-	int i,j,l=65;
+	int i,j,l=1;
 	for(i=1;i<=5;i++)
 	{
 		for(j=1;j<=i;j++)
 		{
+			l++;
 			if(i%2==0)
 			{
-				printf(" %c ",j);
+				printf(" %c ",l+95);
 			}
 			else
 			{
-				printf("%c",l+32);
-				l++;
+				printf(" %c",l+63);
 			}
 		}
 		printf("\n");
